ceb4e6b93d9cb1164dbc9466a347b0fc51b28169
dotDustin <logins.dmahone@gmail.com>
2020-06-29 20:26:33 -0400

    Initial Commit

---
+++ ArtGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtGallery
+//
+//  Created by Dustin Mahone on 6/29/20.
+//  Copyright © 2020 Dustin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
