7c94136b1d0b88b7da5121dd4abe8cea83fc5509
dotDustin <logins.dmahone@gmail.com>
2020-07-01 23:59:27 -0400

    setup core data retrieval

--- ArtGallery/ViewController.swift
+++ ArtGallery/ViewController.swift
@@ -7,20 +7,76 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // MARK: - Outlets
     @IBOutlet weak var tableView: UITableView!
     
+    // MARK: - Variables and Constants
+    var nameArray = [String]()
+    var idArray = [UUID]()
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        getData()
         
     }
 
+    // MARK: - Methods
+    func getData() {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Art")
+        fetchRequest.returnsObjectsAsFaults = false //for efficiency when large amounts of data
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            for result in results as! [NSManagedObject] {
+                if let name = result.value(forKey: "name") as? String {
+                    self.nameArray.append(name)
+                }
+                if let id = result.value(forKey: "id") as? UUID {
+                    self.idArray.append(id)
+                }
+                
+                self.tableView.reloadData()
+            }
+            
+        } catch {
+            print("error")
+        }
+        
+    }
+    
+    // MARK: - Actions
     @objc func addButtonClicked() {
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    // MARK: - Table View
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = nameArray[indexPath.row]
+        
+        return cell
     }
 
 }
